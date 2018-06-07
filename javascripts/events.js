63e7189fa1ad84b2d22b16e6e0c441e8a1d47dfe
Amanda Mitchell <wreedamitchell@gmail.com>
2018-06-06 19:59:30 -0500

    added uid

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,32 +1,49 @@
-const owm = require('./owm');
-const fireBaseApi = require('./fireBaseApi');
-const dom = require('./dom');
+const {showCurrentResults, showFiveDayResults,} = require('./owm');
+const {addWeatherToSaveList,} = require('./fireBaseApi');
+// const dom = require('./dom');
+
+const navLinks = () => {
+  $(document).click((e) => {
+    if (e.target.id === 'authentication') {
+      $('#mySaves').addClass('hide');
+      $('#search').addClass('hide');
+      $('#authScreen').removeClass('hide');
+    } else if (e.target.id === 'mySavedForecast') {
+      $('#mySaves').removeClass('hide');
+      $('#search').addClass('hide');
+      $('#authScreen').addClass('hide');
+    } else if (e.target.id === 'navSearch') {
+      $('#mySaves').addClass('hide');
+      $('#search').removeClass('hide');
+      $('#authScreen').addClass('hide');
+    }
+  });
+};
 
 const pressEnter = () => {
   $(document).keypress((e) => {
     if (e.key === 'Enter') {
       const searchZips = $('#searchBar').val();
-      owm.showCurrentResults(searchZips);
+      showCurrentResults(searchZips);
     }
   });
 };
 
 const today = (e) => {
   $(document).on('click', '.Today', (e) => {
-    owm.showCurrentResults();
+    showCurrentResults();
   });
 };
 
 const fiveDayClick = (e) => {
   $(document).on('click', '.fiveDayButt', (e) => {
-    // $('.fiveDayButt').show();
-    owm.showFiveDayResults();
+    showFiveDayResults();
   });
 };
 
 const saveWeatherEvent = () => {
   $(document).on('click', '.addToSaveList', (e) => {
-    fireBaseApi.addWeatherToSaveList()
+    addWeatherToSaveList()
       .then(() => {
       })
       .catch((error) => {
@@ -35,21 +52,52 @@ const saveWeatherEvent = () => {
   });
 };
 
-const getAllForecastEvent = () => {
-  fireBaseApi.getAllForecast()
-    .then((weatherArray) => {
-      dom.domStrang(weatherArray, 'savedMovies');
-    })
-    .catch((error) => {
-      console.error('error in get all movies', error);
+// const getAllForecastEvent = () => {
+//   fireBaseApi.getAllForecast()
+//     .then((weatherArray) => {
+//       dom.domStrang(weatherArray, 'savedMovies');
+//     })
+//     .catch((error) => {
+//       console.error('error in get all movies', error);
+//     });
+// };
+
+const authEvents = () => {
+  $('#signIn-btn').click((e) => {
+    e.preventDefault();
+    const email = $('#inputEmail').val();
+    const password = $('#inputPassword').val();
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.error('error with signin pg', error);
+      });
+  });
+
+  $('#register-link').click(() => {
+    $('#login-form').addClass('hide');
+    $('#registration-form').removeClass('hide');
+  });
+
+  $('#logIn-link').click(() => {
+    $('#login-form').removeClass('hide');
+    $('#registration-form').addClass('hide');
+  });
+
+  $('#logOut').click(() => {
+    firebase.auth().signOut().then(() => {
+    }).catch((error) => {
+      console.error('error from auth events', error);
     });
+  });
 };
 
 const initializer = () => {
+  navLinks();
   pressEnter();
   today();
   fiveDayClick();
   saveWeatherEvent();
+  authEvents();
 };
 
 module.exports = {
